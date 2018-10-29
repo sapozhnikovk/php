d051a9eaea5b0af3b1d1c23ecf58fa6d0bfe10e4
adm <sapozhnikovk@mail.ru>
2018-10-29 12:36:07 +0300

    сщььше

---
+++ ср01/scripts/sayHelloWeb.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <link href="../../css/phpMM.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div id="header"><h1>PHP & MySQL: The Missing Manual</h1></div>
+        <div id="example">Пример 1.1</div>
+        
+        <div id="content">
+            <h1>Привет, <?php echo $_REQUEST['name']; ?>!</h1>
+            <p>Рады приветствовать вас. Добро пожаловать в начало вашей 
+            одиссеи в мир PHP - программирования.</p>
+        </div>
+        <div id='footer'></div>
+    </body>
+</html>
